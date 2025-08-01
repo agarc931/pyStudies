761f7711d541558a04a4b4fc2d910f352fd02161
Alicia Garcia <76023632+agarc931@users.noreply.github.com>
2025-07-31 20:48:10 -0400

    Initial commit

---
+++ math.py
@@ -0,0 +1,19 @@
+#addition
+x = 2+2
+
+#subtraction
+y = 2-2
+
+#multiplication
+z = 2*2
+
+#division
+a = 2/2
+
+print("sum",x,"\nsubtraction",y,"\nmultiplication",z,"\ndivision",a)
+
+#you can also add strings together
+b = "he"
+c = "llo"
+
+print("you can also add strings together: ",b+c)
---
+++ types.py
@@ -0,0 +1,17 @@
+#variables and types
+x = 2
+y = 3.1
+z = "hello"
+a = True
+
+#x is an integer
+print("Integer: ",x," ", type(x))
+
+#y is a float
+print("Float: ",y," ", type(y))
+
+#z is a string
+print("String: ",z," " ,type(z))
+
+#a is a boolean
+print("Boolean: ",a," " ,type(a))
