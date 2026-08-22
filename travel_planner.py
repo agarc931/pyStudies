f78ab015dda7a0b4ccf83c8865bc32a081b536cf
Alicia Garcia <76023632+agarc931@users.noreply.github.com>
2026-08-22 12:09:06 -0400

    Create travel_planner.py

---
+++ travel_planner.py
@@ -0,0 +1,26 @@
+distance_mi = 1
+is_raining = False
+has_bike = True
+has_car = True
+has_ride_share_app = True
+
+if not distance_mi :
+    print(False)
+
+elif distance_mi <= 1 :
+    if is_raining != True :
+        print(True)
+    else:
+        print(False)
+
+elif distance_mi > 1 and distance_mi <= 6:
+    if has_bike and is_raining != True:
+        print(True)
+    else:
+        print(False)
+
+else:
+    if has_car or has_ride_share_app:
+        print(True)
+    else:
+        print(False)
