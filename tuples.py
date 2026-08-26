5b39dfb94acf010ed5a430e17e20932de853bb1d
Alicia Garcia <76023632+agarc931@users.noreply.github.com>
2026-08-26 07:45:29 -0400

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,4 @@
+## WHAT ARE TUPLES ##
+# A tuple is a Python data type used to create an ordered sequence of 
+# values. Tuples can contain a mixed set of data types like this:
+developer = ('Alice', 34, 'Rust Developer')
