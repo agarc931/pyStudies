086c0df9798a2b12c2ffdf9e91e9d8ffa8671a2d
Alicia Garcia <76023632+agarc931@users.noreply.github.com>
2026-08-28 08:21:05 -0400

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -2,3 +2,14 @@
 # A tuple is a Python data type used to create an ordered sequence of 
 # values. Tuples can contain a mixed set of data types like this:
 developer = ('Alice', 34, 'Rust Developer')
+# I cannot assign a value directly to an index in a tuple. It will give me
+# a type error.
+# To access an index from a tuple you can call it the same way you can
+# in a list.
+developer[1] # 34
+# If you need to access elements starting from the end of a tuple, 
+# then you can use negative indexing.
+numbers = (1,2,3,4,5)
+developer[-2] # 4
+# If you try to pass in an index number that exceeds or equals the length 
+# of the tuple, then you will receive an IndexError
