e73c37797296cd44625c37cb9b32081ff14c0552
Alicia Garcia <76023632+agarc931@users.noreply.github.com>
2026-08-28 17:45:00 -0400

    creating loops.py

---
+++ loops.py
@@ -0,0 +1,16 @@
+# How to use loops
+
+# Loops are used to repeat a block of code. In this lesson, 
+# you will learn how to work with different types of loops in Python.
+
+# the for loop
+programming_languages = ['Rust', 'Java', 'Python', 'C++']
+
+for language in programming_languages:
+    print(language)
+
+# Rust
+# Java
+# Python
+# C++
+
