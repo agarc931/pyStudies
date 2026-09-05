23c1f4590f2d7a7cb47d9956ac8e0b9c615d14f5
Alicia Garcia <76023632+agarc931@users.noreply.github.com>
2026-09-05 03:52:11 -0400

    updating enumerate_zip.py

--- enumerate_zip.py
+++ enumerate_zip.py
@@ -10,6 +10,8 @@ list(enumerate(languages))
 
 # Each entry in the enumerate object (now a list) is a tuple containing a count, followed by a 
 # value from the iterable passed to the enumerate() function.
+print("Using enumerate with default index:")
+print()
 
 languages = ['Spanish', 'English', 'Russian', 'Chinese']
 
@@ -28,10 +30,48 @@ for index, language in enumerate(languages):
 
 # enumerates() takes an optional start argument to set the count to start anywhere other than 0
 #     enumerates(list, #)  -->  syntax
+print()
+print("Using enumerate with index 1:")
+print()
 languages = ['Spanish', 'English', 'Russian', 'Chinese']
 
 for index, language in enumerate(languages, 1):
     print(f'Index {index} and language {language}')
 
+# OUTPUT:
+# Index 1 and language Spanish
+# Index 2 and language English
+# Index 3 and language Russian
+# Index 4 and language Chinese
 
+# zip() - iterates over multiple iterables in parallel.
+#         combines lists into pairs of elements and returns an iterator of tuples.
+developers = ['Naomi' , 'Dario', 'Jessica', 'Tom']
+ids = [1, 2, 3, 4]
 
+list(zip(developers,ids))
+# [('Naomi', 1), ('Dario', 2), ('Jessica', 3), ('Tom', 4)]
+print()
+print("Using zip iterating over devs and ids:")
+print()
+
+# And here's an example of using the zip() function with a for loop to iterate over 
+# developers and ids:
+for name, dev_id in zip(developers, ids):
+    print(f'Name: {name}')
+    print(f'ID: {dev_id}')
+
+# In this example, zip() combines the two lists into pairs of elements and returns 
+# an iterator of tuples. The for loop then unpacks each tuple into name and dev_id. 
+# Finally, for each print statement, we are printing each name and dev_id from the 
+# ids and developers lists respectively. Here is what the result looks like in the console:
+
+# OUTPUT:
+# Name: Naomi
+# ID: 1
+# Name: Dario
+# ID: 2
+# Name: Jessica
+# ID: 3
+# Name: Tom
+# ID: 4
