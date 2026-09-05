ca2eb46747af0621b44f2b4dccec3791e9285aff
Alicia Garcia <76023632+agarc931@users.noreply.github.com>
2026-09-05 04:22:41 -0400

    adding lambda_functions.py

---
+++ lambda_functions.py
@@ -0,0 +1,8 @@
+# Throughout the previous lessons, you have been used to defining functions
+# by using the def keyword like this:
+def square(num):
+    return num ** 2
+
+print(square(4)) # 16
+
+# 
